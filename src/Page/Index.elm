2fbd513d6a6c31637ef8d5a9d8d405ac52915b4c
aaaaargZombies <ahberate@riseup.net>
2023-03-09 16:21:02 +0000

    constrain content with css

--- src/Page/Index.elm
+++ src/Page/Index.elm
@@ -4,7 +4,8 @@ import Html.Styled exposing (Html, div, p, text)
 import I18n.Keys exposing (Key(..))
 import I18n.Translate exposing (translate)
 import Shared exposing (Model, Msg)
-import Theme.PageTemplate as PageTemplate
+import Svg.Styled.Attributes exposing (css)
+import Theme.Global exposing (centerContent)
 
 
 view : Model -> Html Msg
@@ -13,7 +14,7 @@ view model =
         t =
             translate model.language
     in
-    div []
+    div [ css [ centerContent ] ]
         [ p [] [ text (t WelcomeP1) ]
         , p [] [ text (t WelcomeP2) ]
         , p [] [ text (t WelcomeP3) ]
