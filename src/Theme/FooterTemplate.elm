ee6818410a53504e1bfe2de2276b5966d23670b5
Ivan Kocienski <ivan.kocienski@gmail.com>
2023-03-09 17:20:42 +0000

    Footer project text and logo

--- src/Theme/FooterTemplate.elm
+++ src/Theme/FooterTemplate.elm
@@ -1,8 +1,8 @@
 module Theme.FooterTemplate exposing (view)
 
 import Css exposing (Style, batch)
-import Html.Styled exposing (Html, footer, h2, text)
-import Html.Styled.Attributes exposing (css)
+import Html.Styled exposing (Html, footer, h2, text, img, p)
+import Html.Styled.Attributes exposing (css, src)
 import I18n.Keys exposing (Key(..))
 import I18n.Translate exposing (Language(..), translate)
 import Shared exposing (Model, Msg(..))
@@ -15,9 +15,13 @@ view model =
             translate model.language
     in
     footer [ css [ footerStyle ] ]
-        [ h2
+        [ img [ src "/images/dfl-logo.svg" ] []
+        , h2
             []
             [ text (t FooterText)
+            ]
+        , p []
+            [ text (t ProjectInfo)
             ]
         ]
 
