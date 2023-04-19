fab70815edeb2c0b2540583fe41859c2ba34f148
aaaaargZombies <ahberate@riseup.net>
2023-04-19 14:19:39 +0100

    add teaser list inlcuding remote resources to Guides view

--- src/Page/Guides.elm
+++ src/Page/Guides.elm
@@ -5,10 +5,13 @@ import Html.Styled exposing (Html, a, div, h1, li, text, ul)
 import Html.Styled.Attributes exposing (class, css, href)
 import I18n.Keys exposing (Key(..))
 import I18n.Translate exposing (translate)
+import List exposing (concat)
 import Message exposing (Msg)
 import Page.Guide.Data exposing (Guide)
+import Page.Shared exposing (GuideTeaser)
+import Page.Shared.View
 import Route
-import Shared exposing (Model)
+import Shared exposing (Model, Request(..))
 import Theme.Global exposing (centerContent)
 
 
@@ -18,17 +21,21 @@ view model =
         t =
             translate model.language
 
-        guides =
-            List.map
-                guideCard
-                (List.sortBy
-                    .title
-                    (Dict.values model.content.guides)
-                )
+        teaserList : List GuideTeaser
+        teaserList =
+            case model.externalActions of
+                Failure ->
+                    Page.Guide.Data.teaserListFromGuideDict model.language model.content.guides
+
+                Loading ->
+                    Page.Guide.Data.teaserListFromGuideDict model.language model.content.guides
+
+                Success list ->
+                    concat [ Page.Guide.Data.teaserListFromGuideDict model.language model.content.guides, list ]
     in
     div [ css [ centerContent ] ]
         [ h1 [] [ text (t GuidesTitle) ]
-        , ul [] guides
+        , Page.Shared.View.viewGuideTeaserList teaserList
         ]
 
 
