5579c4524895b76ad9d87254ef2f56e4dde1e2b1
aaaaargZombies <ahberate@riseup.net>
2023-04-19 13:50:42 +0100

    WIP - display external actions as GuideTeasers, not yet linking

--- src/Page/Index.elm
+++ src/Page/Index.elm
@@ -4,10 +4,12 @@ import Html.Styled exposing (Html, div, p, text)
 import Html.Styled.Attributes exposing (css)
 import I18n.Keys exposing (Key(..))
 import I18n.Translate exposing (translate)
+import List exposing (concat)
 import Message exposing (Msg)
 import Page.Guide.Data
+import Page.Shared exposing (GuideTeaser)
 import Page.Shared.View
-import Shared exposing (Model)
+import Shared exposing (Model, Request(..))
 import Theme.Global exposing (centerContent)
 
 
@@ -16,11 +18,22 @@ view model =
     let
         t =
             translate model.language
+
+        teaserList : List GuideTeaser
+        teaserList =
+            case model.externalActions of
+                Failure ->
+                    Page.Guide.Data.teaserListFromGuideDict model.language model.content.guides
+
+                Loading ->
+                    Page.Guide.Data.teaserListFromGuideDict model.language model.content.guides
+
+                Success list ->
+                    concat [ Page.Guide.Data.teaserListFromGuideDict model.language model.content.guides, list ]
     in
     div [ css [ centerContent ] ]
         [ p [] [ text (t WelcomeP1) ]
         , p [] [ text (t WelcomeP2) ]
         , p [] [ text (t WelcomeP3) ]
-        , Page.Guide.Data.teaserListFromGuideDict model.language model.content.guides
-            |> Page.Shared.View.viewGuideTeaserList
+        , Page.Shared.View.viewGuideTeaserList teaserList
         ]
