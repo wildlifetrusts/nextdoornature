6d1807111e84cc8efa7ce1a77501f4ef86256c6e
aaaaargZombies <ahberate@riseup.net>
2023-04-19 14:23:47 +0100

    remove external actions from index page

--- src/Page/Index.elm
+++ src/Page/Index.elm
@@ -4,10 +4,8 @@ import Html.Styled exposing (Html, div, p, text)
 import Html.Styled.Attributes exposing (css)
 import I18n.Keys exposing (Key(..))
 import I18n.Translate exposing (translate)
-import List exposing (concat)
 import Message exposing (Msg)
 import Page.Guide.Data
-import Page.Shared exposing (GuideTeaser)
 import Page.Shared.View
 import Shared exposing (Model, Request(..))
 import Theme.Global exposing (centerContent)
@@ -18,22 +16,11 @@ view model =
     let
         t =
             translate model.language
-
-        teaserList : List GuideTeaser
-        teaserList =
-            case model.externalActions of
-                Failure ->
-                    Page.Guide.Data.teaserListFromGuideDict model.language model.content.guides
-
-                Loading ->
-                    Page.Guide.Data.teaserListFromGuideDict model.language model.content.guides
-
-                Success list ->
-                    concat [ Page.Guide.Data.teaserListFromGuideDict model.language model.content.guides, list ]
     in
     div [ css [ centerContent ] ]
         [ p [] [ text (t WelcomeP1) ]
         , p [] [ text (t WelcomeP2) ]
         , p [] [ text (t WelcomeP3) ]
-        , Page.Shared.View.viewGuideTeaserList teaserList
+        , Page.Guide.Data.teaserListFromGuideDict model.language model.content.guides
+            |> Page.Shared.View.viewGuideTeaserList
         ]
