b73563854c08bc934891de0577c5a5f02e801faa
Rosemary Ferrier <rosemary.ferrier@gmail.com>
2023-04-24 16:27:53 +0100

    style header & move translate button into header

--- src/Theme/HeaderTemplate.elm
+++ src/Theme/HeaderTemplate.elm
@@ -1,19 +1,20 @@
 module Theme.HeaderTemplate exposing (view)
 
-import Css exposing (Style, batch, fontFamilies, fontSize, margin, rem, zero)
-import Html.Styled exposing (Html, a, div, h1, header, input, label, node, text)
+import Css exposing (Style, alignItems, baseline, batch, center, column, displayFlex, flexDirection, flexEnd, flexStart, flexWrap, fontFamilies, fontSize, justifyContent, left, margin, margin2, marginBottom, marginRight, marginTop, noWrap, rem, right, row, spaceBetween, textAlign, zero)
+import Html.Styled exposing (Html, a, button, div, h1, header, input, label, node, text)
 import Html.Styled.Attributes exposing (attribute, css, href, id, placeholder, property, type_)
-import Html.Styled.Events exposing (on)
+import Html.Styled.Events exposing (on, onClick)
 import I18n.Keys exposing (Key(..))
 import I18n.Translate exposing (Language(..), translate)
 import Json.Decode
 import List
-import Message exposing (Msg)
+import Message exposing (Msg(..))
 import Page.Guide.Data
 import Page.GuideTeaser
 import Page.Shared.View
 import Route exposing (Route(..))
 import Shared exposing (Model, Request(..))
+import Theme.Global exposing (centerContent, outerPadding, withMediaMobileUp)
 
 
 view : Model -> Html Msg
@@ -23,24 +24,28 @@ view model =
         t =
             translate model.language
     in
-    header []
+    header [ css [ headerContainerStyle ] ]
         [ viewSiteTitle model.page (t SiteTitle)
-        , case model.page of
-            Guides ->
-                searchInput model
+        , div [ css [ searchButtonsContainerStyle ] ]
+            [ button [ onClick LanguageChangeRequested ]
+                [ text (t ChangeLanguage) ]
+            , case model.page of
+                Guides ->
+                    searchInput model
 
-            _ ->
-                a [ href "/guides" ] [ text <| t FooterGuidesLinkText ]
+                _ ->
+                    a [ href "/guides", css [ headerLinkStyle ] ] [ text <| t FooterGuidesLinkText ]
+            ]
         ]
 
 
 viewSiteTitle : Route -> String -> Html Msg
 viewSiteTitle route siteTitle =
     if route == Index then
-        h1 [ css [ headerBrandStyle ] ] [ text siteTitle ]
+        h1 [ css [ headerBrandStyle, headerTitleStyle ] ] [ text siteTitle ]
 
     else
-        div [ css [ headerBrandStyle ] ]
+        div [ css [ headerBrandStyle, headerTitleStyle ] ]
             [ a [ href "/" ]
                 [ text siteTitle
                 ]
@@ -85,4 +90,55 @@ headerBrandStyle =
         [ fontSize (rem 4)
         , fontFamilies [ "Ludicrous" ]
         , margin zero
+        ]
+
+
+headerContainerStyle : Style
+headerContainerStyle =
+    batch
+        [ alignItems baseline
+        , centerContent
+        , displayFlex
+        , flexDirection column
+        , flexWrap noWrap
+        , justifyContent center
+        , outerPadding
+        , withMediaMobileUp
+            [ flexDirection row
+            , justifyContent spaceBetween
+            ]
+        ]
+
+
+searchButtonsContainerStyle : Style
+searchButtonsContainerStyle =
+    batch
+        [ alignItems flexStart
+        , displayFlex
+        , flexDirection column
+        , flexWrap noWrap
+        , justifyContent center
+        , marginTop (rem 2)
+        , marginBottom (rem 0)
+        , withMediaMobileUp
+            [ margin2 (rem 2) (rem 0)
+            , alignItems flexEnd
+            ]
+        ]
+
+
+headerLinkStyle : Style
+headerLinkStyle =
+    batch
+        [ textAlign left
+        , withMediaMobileUp
+            [ textAlign right
+            ]
+        ]
+
+
+headerTitleStyle : Style
+headerTitleStyle =
+    batch
+        [ marginRight (rem 3)
         ]
