074ce57ecd724207e497b400f436d8ff27d58c76
katjam <katjamordaunt@gmail.com>
2023-04-28 13:53:11 +0100

    WIP begin get content by language refactor

--- src/Metadata.elm
+++ src/Metadata.elm
@@ -1,7 +1,8 @@
 port module Metadata exposing (metadataFromPage, setMetadata)
 
+import Dict
 import I18n.Keys exposing (Key(..))
-import I18n.Translate exposing (Language(..), translate)
+import I18n.Translate exposing (Language(..), dictFromLanguage, translate)
 import Route exposing (Route(..))
 import Shared exposing (Content)
 
@@ -10,6 +11,16 @@ type alias PageMetadata =
     { title : String
     , description : String
     }
+
+
+defaultMetadata : Language -> PageMetadata
+defaultMetadata language =
+    let
+        t : Key -> String
+        t =
+            translate language
+    in
+    { title = t SiteTitle, description = t WelcomeP1 }
 
 
 metadataFromPage : Route -> Language -> Content -> PageMetadata
@@ -26,9 +37,12 @@ metadataFromPage page language content =
             }
 
         Guide slug ->
-            { title = subPageTitle language slug
-            , description = slug
-            }
+            case metadataFromSlug slug (dictFromLanguage language content.guides) of
+                Just metadata ->
+                    { metadata | title = subPageTitle language metadata.title }
+
+                Nothing ->
+                    defaultMetadata language
 
         Guides ->
             { title = subPageTitle language (t GuidesTitle)
@@ -46,14 +60,17 @@ metadataFromPage page language content =
             }
 
 
-metadataFromSlug : ( Route, String ) -> PageMetadata
-metadataFromSlug ( page, slug ) =
-    --case Dict.get slug pages of
-    --     Just aPage ->
-    --         aPage.title
-    --        Nothing ->
-    --        ""
-    { title = slug, description = slug }
+metadataFromSlug :
+    String
+    -> Dict.Dict String { a | summary : String, title : String }
+    -> Maybe PageMetadata
+metadataFromSlug slug contentDict =
+    case Dict.get slug contentDict of
+        Just content ->
+            Just { title = content.title, description = content.summary }
+
+        Nothing ->
+            Nothing
 
 
 subPageTitle : Language -> String -> String
