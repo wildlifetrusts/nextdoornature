b2b9e3e8bd3c610c32bd09c92500b8da6523b63a
aaaaargZombies <ahberate@riseup.net>
2023-06-08 07:35:59 +0100

    basic layout of submitStory page

--- src/Page/SubmitStory/View.elm
+++ src/Page/SubmitStory/View.elm
@@ -1,6 +1,6 @@
 module Page.SubmitStory.View exposing (..)
 
-import Css exposing (Style, batch, hidden, maxWidth, overflow, pct, property, width)
+import Css exposing (Style, auto, batch, display, displayFlex, flex3, flexBasis, flexGrow, flexShrink, flexWrap, hidden, int, maxWidth, overflow, pct, property, width, wrap)
 import Css.Transitions exposing (objectPosition2)
 import Html.Styled exposing (Html, div, h2, iframe, img, li, p, text, ul)
 import Html.Styled.Attributes as Attr exposing (attribute, css)
@@ -14,7 +14,22 @@ view : Model -> Html Msg
 view model =
     div [ css [ centerContent ] ]
         [ primaryHeader [] "Share your story"
-        , description
+        , submitFlex model
+        ]
+
+
+submitFlex : Model -> Html Msg
+submitFlex model =
+    div
+        [ css
+            [ batch
+                [ displayFlex
+                , property "gap" "1rem"
+                , flexWrap wrap
+                ]
+            ]
+        ]
+        [ description
         , form model
         , images
         ]
@@ -22,7 +37,7 @@ view model =
 
 description : Html Msg
 description =
-    div []
+    div [ css [ flexChild ] ]
         [ p [] [ text "[cCc] Case studies are a vital tool  in helping others achieve success - Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni esse sequi iste eligendi nobis, pariatur repellat itaque voluptatum ab ad eos animi, asperiores dolores repellendus? Tenetur, minima! Fugiat, soluta corrupti?" ]
         , p [] [ text "[cCc] By submitting  a case study Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni esse sequi iste eligendi nobis, pariatur repellat itaque voluptatum ab ad eos animi, asperiores dolores repellendus? Tenetur, minima! Fugiat, soluta corrupti?" ]
         ]
@@ -54,7 +69,7 @@ form model =
                         ]
                         []
     in
-    div [ css [ batch [ width (pct 100) ] ] ]
+    div [ css [ batch [ width (pct 100) ], flexChild ] ]
         [ h2 [] [ text "your details" ]
         , formstack
         ]
@@ -62,12 +77,12 @@ form model =
 
 images : Html Msg
 images =
-    ul [ attribute "role" "list", css [ listStyleNone ] ]
-        [ li [] [ img [ css [ roundedCornerStyle, submitImgStyle ], Attr.src "./public/images/submitStory/david-clode-eL4ADAsiOR8-unsplash.jpg", Attr.alt "" ] [] ]
-        , li [] [ img [ css [ roundedCornerStyle, submitImgStyle ], Attr.src "./public/images/submitStory/steve-adams-bY_q4VodUc0-unsplash.jpg", Attr.alt "" ] [] ]
-        , li [] [ img [ css [ roundedCornerStyle, submitImgStyle ], Attr.src "./public/images/submitStory/kenny-eliason-qo6_mo9dsYg-unsplash.jpg", Attr.alt "" ] [] ]
-        , li [] [ img [ css [ roundedCornerStyle, submitImgStyle ], Attr.src "./public/images/submitStory/sincerely-media-Agr1YTrzYPI-unsplash.jpg", Attr.alt "" ] [] ]
-        , li [] [ img [ css [ roundedCornerStyle, submitImgStyle ], Attr.src "./public/images/submitStory/ocg-saving-the-ocean-bWAArZ5M4Ag-unsplash.jpg", Attr.alt "" ] [] ]
+    ul [ attribute "role" "list", css [ listStyleNone, submitImgGrid, flexChild ] ]
+        [ li [] [ img [ css [ roundedCornerStyle, submitImgStyle ], Attr.src "/images/submitStory/david-clode-eL4ADAsiOR8-unsplash.jpg", Attr.alt "" ] [] ]
+        , li [] [ img [ css [ roundedCornerStyle, submitImgStyle ], Attr.src "/images/submitStory/steve-adams-bY_q4VodUc0-unsplash.jpg", Attr.alt "" ] [] ]
+        , li [] [ img [ css [ roundedCornerStyle, submitImgStyle ], Attr.src "/images/submitStory/kenny-eliason-qo6_mo9dsYg-unsplash.jpg", Attr.alt "" ] [] ]
+        , li [] [ img [ css [ roundedCornerStyle, submitImgStyle ], Attr.src "/images/submitStory/sincerely-media-Agr1YTrzYPI-unsplash.jpg", Attr.alt "" ] [] ]
+        , li [] [ img [ css [ roundedCornerStyle, submitImgStyle ], Attr.src "/images/submitStory/ocg-saving-the-ocean-bWAArZ5M4Ag-unsplash.jpg", Attr.alt "" ] [] ]
         ]
 
 
@@ -78,4 +93,22 @@ submitImgStyle =
         , property "aspect-ratio" "9/6"
         , overflow hidden
         , property "object-position" "center bottom"
+        ]
+
+
+submitImgGrid : Style
+submitImgGrid =
+    batch
+        [ property "display" "grid"
+        , property "grid-template-columns" "repeat(auto-fit, minmax(280px, 1fr))"
+        , property "gap" "1rem"
+        ]
+
+
+flexChild : Style
+flexChild =
+    batch
+        [ flexShrink (int 1)
+        , flexGrow (int 1)
+        , property "flex-basis" "25rem"
         ]
