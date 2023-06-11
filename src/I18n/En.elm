8b8d7798ec18b7154cb0cfd612396f73625701d3
aaaaargZombies <ahberate@riseup.net>
2023-06-11 20:39:13 +0100

    add approved copy

--- src/I18n/En.elm
+++ src/I18n/En.elm
@@ -174,13 +174,18 @@ enStrings key =
         -- SubmitStory Page
         ---
         SubmitStoryHeading ->
-            "[cCc] Share your story"
+            "Share your story!"
 
         SubmitStoryP1 ->
-            "[cCc] Case studies are a vital tool  in helping others achieve success - Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni esse sequi iste eligendi nobis, pariatur repellat itaque voluptatum ab ad eos animi, asperiores dolores repellendus? Tenetur, minima! Fugiat, soluta corrupti?"
+            """
+            Have you been inspired to take action for nature in your area? Tell everyone about it here! Your project can be small or large, finished or ongoing. You can give contact details or you can keep the location very vague and just say "Yorkshire" or "Salisbury". The most important thing is to share the useful information that will help others. What went well? What mistakes did you learn from?
+"""
 
         SubmitStoryP2 ->
-            "[cCc] By submitting  a case study Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni esse sequi iste eligendi nobis, pariatur repellat itaque voluptatum ab ad eos animi, asperiores dolores repellendus? Tenetur, minima! Fugiat, soluta corrupti?"
+            """
+          By sending us your story, you are consenting to making it public on this website. You must make sure that everyone in the photo has agreed to this. Do not send us anything that is private. We might edit your text to make it fit. By sending us your story, you are agreeing to let us contact you about this story, but you can opt out of contact at any time. If you have any questions about how your data is stored, how to opt out, and what we do with your data, please see our Privacy policy or email nextdoornature@wildlifetrusts.org
+          """
+
         SubmitFormSrc ->
             "https://teamwildertest.formstack.com/forms/teamwilder_test"
 
