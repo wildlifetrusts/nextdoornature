58a79ee72da83d7d481be960660b9b16680cd859
ivan <ivan@localhost.localdomain>
2023-06-13 15:45:42 +0100

    Stories have summary field, default value ""

--- src/Page/Story/Data.elm
+++ src/Page/Story/Data.elm
@@ -15,6 +15,7 @@ type alias Stories =
 type alias Story =
     { title : String
     , slug : String
+    , summary : String
     , maybeLocation : Maybe String
     , maybeGroupOrIndividual : Maybe String
     , images : List Image
@@ -51,6 +52,8 @@ storyDictDecoder =
                 (Json.Decode.field "title" Json.Decode.string |> Json.Decode.Extra.withDefault "")
             |> Json.Decode.Extra.andMap
                 (Json.Decode.field "basename" Json.Decode.string |> Json.Decode.Extra.withDefault "")
+            |> Json.Decode.Extra.andMap
+                (Json.Decode.field "summary" Json.Decode.string |> Json.Decode.Extra.withDefault "")
             |> Json.Decode.Extra.andMap
                 (Json.Decode.maybe (Json.Decode.field "location" Json.Decode.string |> Json.Decode.Extra.withDefault ""))
             |> Json.Decode.Extra.andMap
