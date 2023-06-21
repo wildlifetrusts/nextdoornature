3646f9681d513e6bde86d2c7a21ed876881811c9
Rosemary Ferrier <rosemary.ferrier@gmail.com>
2023-06-21 16:07:30 +0100

    improve layout of 404 page

--- src/Page/View.elm
+++ src/Page/View.elm
@@ -1,26 +1,16 @@
 module Page.View exposing (view)
 
-import Css exposing (Style, batch, maxWidth, minHeight, px)
 import Html.Styled exposing (Html, div)
 import Html.Styled.Attributes exposing (css)
 import Message exposing (Msg)
 import Page.Data
-import Theme.Global exposing (centerContent, maxTabletPortrait, primaryHeader)
+import Theme.Global exposing (centerContent, primaryHeader, simplePageContentContainerStyle)
 import Theme.Markdown exposing (markdownToHtml)
 
 
 view : Page.Data.Page -> Html Msg
 view page =
-    div [ css [ centerContent, ancillaryPageTextContainerStyle ] ]
+    div [ css [ centerContent, simplePageContentContainerStyle ] ]
         [ primaryHeader [] page.title
         , div [] (markdownToHtml page.fullTextMarkdown)
-        ]
-
-
-ancillaryPageTextContainerStyle : Style
-ancillaryPageTextContainerStyle =
-    batch
-        [ maxWidth (px maxTabletPortrait)
-        , minHeight
-            (px 300)
         ]
--- src/Theme/Global.elm
+++ src/Theme/Global.elm
@@ -1,6 +1,6 @@
-module Theme.Global exposing (borderWrapper, centerContent, contentWrapper, featureImageStyle, globalStyles, hideFromPrint, lightPurple, lightTeal, listStyleNone, maxTabletPortrait, mediumTeal, pageColumnStyle, primaryHeader, purple, roundedCornerStyle, screenReaderOnly, simpleThreeColumnFlexChildStyle, simpleThreeColumnFlexStyle, teal, teaserContainerStyle, teaserImageStyle, teaserRowStyle, topTwoColumnsWrapperStyle, white, withMediaDesktopUp, withMediaMobileUp, withMediaPrint, withMediaTabletLandscapeUp, withMediaTabletPortraitUp)
+module Theme.Global exposing (borderWrapper, centerContent, contentWrapper, featureImageStyle, globalStyles, hideFromPrint, lightPurple, lightTeal, listStyleNone, maxTabletPortrait, mediumTeal, pageColumnStyle, primaryHeader, purple, roundedCornerStyle, screenReaderOnly, simplePageContentContainerStyle, simpleThreeColumnFlexChildStyle, simpleThreeColumnFlexStyle, teal, teaserContainerStyle, teaserImageStyle, teaserRowStyle, topTwoColumnsWrapperStyle, white, withMediaDesktopUp, withMediaMobileUp, withMediaPrint, withMediaTabletLandscapeUp, withMediaTabletPortraitUp)
 
-import Css exposing (Color, Style, absolute, alignItems, auto, backgroundColor, backgroundImage, backgroundPosition, backgroundRepeat, backgroundSize, batch, border, border3, borderBottomRightRadius, borderRadius4, borderTopLeftRadius, borderTopRightRadius, boxSizing, breakWord, center, cm, color, column, contentBox, cover, cursor, display, displayFlex, flex, flex3, flexDirection, flexGrow, flexShrink, flexStart, flexWrap, fontFamilies, height, hex, hidden, hover, inherit, int, justifyContent, lastChild, left, listStyle, margin, margin2, margin3, marginBottom, marginRight, maxWidth, minWidth, noRepeat, noWrap, none, overflow, overflowWrap, padding, padding2, paddingLeft, pct, pointer, position, property, pseudoElement, px, rem, row, solid, textDecoration, textDecoration3, top, underline, width, wrap, zero)
+import Css exposing (Color, Style, absolute, alignItems, auto, backgroundColor, backgroundImage, backgroundPosition, backgroundRepeat, backgroundSize, batch, border, border3, borderBottomRightRadius, borderRadius4, borderTopLeftRadius, borderTopRightRadius, boxSizing, breakWord, center, cm, color, column, contentBox, cover, cursor, display, displayFlex, flex, flex3, flexDirection, flexGrow, flexShrink, flexStart, flexWrap, fontFamilies, height, hex, hidden, hover, inherit, int, justifyContent, lastChild, left, listStyle, margin, margin2, margin3, marginBottom, marginRight, maxWidth, minHeight, minWidth, noRepeat, noWrap, none, overflow, overflowWrap, padding, padding2, paddingLeft, pct, pointer, position, property, pseudoElement, px, rem, row, solid, textDecoration, textDecoration3, top, underline, width, wrap, zero)
 import Css.Global exposing (global, typeSelector)
 import Css.Media as Media exposing (only, print, screen, withMedia)
 import Html.Styled exposing (Html, h1, text)
@@ -427,4 +427,13 @@ simpleThreeColumnFlexChildStyle =
         [ flexShrink (int 1)
         , flexGrow (int 1)
         , property "flex-basis" "25rem"
+        ]
+
+
+simplePageContentContainerStyle : Style
+simplePageContentContainerStyle =
+    batch
+        [ maxWidth (px maxTabletPortrait)
+        , minHeight
+            (px 300)
         ]
